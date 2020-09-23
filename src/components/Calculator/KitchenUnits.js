3d50324a5bac27c8fa1e46beb49eb187982c8fb7
kostyrko <mkostyrko@gmail.com>
2020-09-23 10:46:47 +0200

    Change DB to objects

--- src/components/Calculator/KitchenUnits.js
+++ src/components/Calculator/KitchenUnits.js
@@ -2,35 +2,60 @@ import React, {useState} from 'react';
 import SelectOption from './SelectOption'
 
 export const KitchenUnits = () => {
-  const [upperUnits, setUpperUnits] = useState({
-    "typ 1": 0,
-    "typ 2": 0
+  const [upperUnits, setUpperUnits] = useState({ 
+    type1 : {
+      name: 'typ-1',
+      quantity : 0,
+      price: 600,
+    },
+    type2 : {
+      name: 'typ-2',
+      quantity : 0,
+      price: 500,
+    }
   })
   const [lowerUnits, setLowerUnits] = useState({
-    "typ 3": 0,
-    "typ 4": 0
+    type3 : {
+      name: 'typ-3',
+      quantity : 0,
+      price: 400,
+    },
+    type4 : {
+      name: 'typ-4',
+      quantity : 0,
+      price: 300,
+    }
   })
   // const {type1,type2} = upperUnits
+
+  
   const upperUnitTypes = Object.keys(upperUnits)
   const lowerUnitTypes = Object.keys(lowerUnits)
 
   const handleNumChange = e => {
     const {name, value} = e.target;
+    
     if (upperUnitTypes.includes(name)) {
       setUpperUnits({
         ...upperUnits,
-        [name] : parseInt(value)
+        [name] : {
+          ...upperUnits[name],
+          quantity: parseInt(value)
+        }
       })
     } else {
       setLowerUnits({
         ...lowerUnits,
-        [name] : parseInt(value)
+        [name] : {
+          ...lowerUnits[name],
+          quantity: parseInt(value)
+        }
       })
     }
   }
   
-  // console.log(upperUnits, lowerUnits);
-  // console.log(upperUnits.typ_1);
+  console.log('upperUnits',upperUnits);
+  console.log('lowerUnits',lowerUnits);
 
   return (
     <div>
@@ -38,7 +63,8 @@ export const KitchenUnits = () => {
       <div className="options">
         {
           upperUnitTypes.map((elem,i)=> {
-            return <SelectOption key={i} type={elem} handleNumChange={handleNumChange} selectedNum={upperUnits[elem]}/>
+            const {name, quantity, price} = upperUnits[elem]
+            return <SelectOption type={elem} key={i} name={name} handleNumChange={handleNumChange} selectedNum={quantity} price={price}/>
           })
         }
       </div>
@@ -46,9 +72,30 @@ export const KitchenUnits = () => {
       <div className="options">
         {
           lowerUnitTypes.map((elem,i)=> {
-            return <SelectOption key={i} type={elem} handleNumChange={handleNumChange} selectedNum={lowerUnits[elem]}/>
+            const {name, quantity, price} = lowerUnits[elem]
+            return <SelectOption type={elem} key={i} name={name} handleNumChange={handleNumChange} selectedNum={quantity} price={price}/>
+
           })
         }
+      </div>
+
+      <div className="units-drawing">
+        <p>Typ 1</p>
+        <p>szafka nr ...</p>
+      </div>
+
+      <div className="chosen-units">
+        <ul className="units-list">
+          <p>Typ 1</p>
+          <p>ilość {upperUnits["type1"].quantity}</p>
+
+          <li>
+            <p>Szafka nr ...</p>
+            <label htmlFor="width">Szerokość</label>
+            <input name="width" type="number"/>
+            <p>cena: cena zmienna i zależna od szerokości</p>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/components/Calculator/SelectOption.js
+++ src/components/Calculator/SelectOption.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const SelectOption = ({type,handleNumChange, selectedNum}) => {
+const SelectOption = ({type, name,handleNumChange, selectedNum, price}) => {
   // console.log('selectedNum', selectedNum);
   return (
     <div className="option1">
-          <h3>Szafka {type}</h3>
-          <img src="" alt={type}/>
+          <h3>Szafka {name}</h3>
+          <p>Cena wyjściowa: {price} pln</p>
+          <img src="https://github.com/kostyrko/flacky-meble/blob/dev/img/typ1.jpg?raw=true" alt={name}/>
           <select name={type} value={selectedNum} onChange={handleNumChange}>
             <option value="">Wybierz ilość</option>
             <option value="1">1</option>
