d5254b409276f6cb8cd209b60fbef0f0317617ec
kostyrko <mkostyrko@gmail.com>
2020-10-13 12:19:24 +0200

    Add 3rd wall

--- src/components/Calculator/WallsOption.js
+++ src/components/Calculator/WallsOption.js
@@ -5,8 +5,9 @@ const WallsOption = ({ onChange }) => {
     <div className='walls-option'>
       <label htmlFor="wallNum">Ilość ścian do zabudowy</label>
       <div onChange={(e) => onChange(e)}>
-        <input type="radio" value={1} name="wallNum" /> 1
+        <input type="radio" value={1} name="wallNum" checked="checked" /> 1
         <input type="radio" value={2} name="wallNum" /> 2
+        <input type="radio" value={3} name="wallNum" /> 3
       </div>
     </div>
   );
